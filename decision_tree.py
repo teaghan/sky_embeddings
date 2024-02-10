6cbfd652a284c8aed8324e1f7b08951efb31121c
Teaghan O'Briain <obriaintb@gmail.com>
2024-02-10 20:56:20 +0000

    regression

--- decision_tree.py
+++ decision_tree.py
@@ -6,14 +6,14 @@ import numpy as np
 import h5py
 import ast
 
-from utils.pretrain import str2bool
+from utils.pretrain_simmim import str2bool
 from utils.models_simmim import build_model
 from utils.dataloader_simmim import build_dataloader
 from utils.similarity import mae_latent
 
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.metrics import accuracy_score, mean_squared_error, r2_score
 
 def parseArguments():
     # Create argument parser
@@ -23,8 +23,10 @@ def parseArguments():
     parser.add_argument("model_names", help="Names of models to test.")
 
     # Optional arguments
-    parser.add_argument("-fn", "--data_fn", 
+    parser.add_argument("-cfn", "--class_data_fn", 
                         type=str, default='simple_classifier_data.h5')
+    parser.add_argument("-rfn", "--regress_data_fn", 
+                        type=str, default='simple_regression_data.h5')
     parser.add_argument("-c", "--combine", 
                         type=str, default='pool')
     
@@ -35,40 +37,7 @@ def parseArguments():
     
     return parser
 
-# Determine device
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-n_gpu = torch.cuda.device_count()
-print(f'Using Torch version: {torch.__version__}')
-print(f'Using a {device} device with {n_gpu} GPU(s)')
-
-args = parseArguments()
-args = args.parse_args()
-
-model_names = ast.literal_eval(args.model_names)
-scores = []
-for model_name in model_names:
-    data_fn = args.data_fn
-    combine = args.combine
-    
-    # Directories
-    cur_dir = os.path.dirname(__file__)
-    config_dir = os.path.join(cur_dir, 'configs/')
-    model_dir = os.path.join(cur_dir, 'models/')
-    data_dir = args.data_dir
-    if data_dir is None:
-        data_dir = os.path.join(cur_dir, 'data/')
-    results_dir = os.path.join(cur_dir, 'results/')
-    if not os.path.exists(results_dir):
-        os.mkdir(results_dir)
-    
-    # Load model configuration
-    config = configparser.ConfigParser()
-    config.read(config_dir+model_name+'.ini')
-    
-    # Construct the model and load pretrained weights
-    model_filename =  os.path.join(model_dir, model_name+'.pth.tar') 
-    model, losses, cur_iter = build_model(config, model_filename, device, build_optimizer=False)
-    
+def get_embeddings(data_path, config, model, device, y_label='class', combine='pool'):
     # Data loader stuff
     num_workers = min([os.cpu_count(),12*n_gpu])
     if config['DATA']['norm_type']=='global':
@@ -77,9 +46,8 @@ for model_name in model_names:
     else:
         pix_mean = None
         pix_std = None
-    
     # Data loader
-    dataloader = build_dataloader(os.path.join(data_dir, data_fn), 
+    dataloader = build_dataloader(data_path, 
                                          norm_type=config['DATA']['norm_type'], 
                                          batch_size=64, 
                                          num_workers=num_workers,
@@ -92,37 +60,108 @@ for model_name in model_names:
                                          num_channels=int(config['ARCHITECTURE']['num_channels']), 
                                          max_mask_ratio=None,
                                          shuffle=False)
-    
+
     # Map target samples to latent-space
     latent_features = mae_latent(model, dataloader, device)
     latent_features = latent_features.data.cpu().numpy()
-    
-    with h5py.File(os.path.join(data_dir, data_fn), "r") as f:
-        y = f['class'][:]
-    
+
+    # Collect targets
+    with h5py.File(data_path, "r") as f:
+        y = f[y_label][:]
+
     if combine=='flatten':
         x = latent_features.reshape(latent_features.shape[0], -1)
     elif combine=='pool':
         x = np.max(latent_features, axis=1)
     else:
         x = np.mean(latent_features, axis=1)
+
+    return x, y
+
+if __name__=="__main__":
+    # Determine device
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    n_gpu = torch.cuda.device_count()
+    print(f'Using Torch version: {torch.__version__}')
+    print(f'Using a {device} device with {n_gpu} GPU(s)')
     
-    # Splitting the dataset into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    args = parseArguments()
+    args = args.parse_args()
     
-    # Creating and training a decision tree classifier
-    clf = DecisionTreeClassifier(max_depth=None, random_state=42)
-    clf.fit(X_train, y_train)
-    
-    # Predicting the class label
-    y_pred_test = clf.predict(X_test)
-    y_pred_train = clf.predict(X_train)
-    
-    # Evaluating the classifier
-    test_accuracy = accuracy_score(y_test, y_pred_test)
-    train_accuracy = accuracy_score(y_train, y_pred_train)
-    print(f'{model_name}:') 
-    print(f'\t Train accuracy: {train_accuracy:.4f}')
-    print(f'\t Test accuracy: {test_accuracy:.4f}')
-    scores.append(test_accuracy)
-print(f'Best model: {model_names[np.argmax(scores)]}')
+    model_names = ast.literal_eval(args.model_names)
+    acc_scores = []
+    regress_scores = []
+    for model_name in model_names:
+        
+        # Directories
+        cur_dir = os.path.dirname(__file__)
+        config_dir = os.path.join(cur_dir, 'configs/')
+        model_dir = os.path.join(cur_dir, 'models/')
+        data_dir = args.data_dir
+        if data_dir is None:
+            data_dir = os.path.join(cur_dir, 'data/')
+        results_dir = os.path.join(cur_dir, 'results/')
+        if not os.path.exists(results_dir):
+            os.mkdir(results_dir)
+        
+        # Load model configuration
+        config = configparser.ConfigParser()
+        config.read(config_dir+model_name+'.ini')
+        
+        # Construct the model and load pretrained weights
+        model_filename =  os.path.join(model_dir, model_name+'.pth.tar') 
+        model, losses, cur_iter = build_model(config, model_filename, device, build_optimizer=False)
+
+        # Classifier task
+        class_data_fn = args.class_data_fn
+        combine = args.combine
+        x,y = get_embeddings(os.path.join(data_dir, class_data_fn), 
+                             config, model, device, y_label='class', combine=combine)
+        
+        # Splitting the dataset into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+        
+        # Creating and training a decision tree classifier
+        clf = DecisionTreeClassifier(max_depth=None, random_state=42) #12
+        clf.fit(X_train, y_train)
+        
+        # Predicting the class label
+        y_pred_test = clf.predict(X_test)
+        y_pred_train = clf.predict(X_train)
+        
+        # Evaluating the classifier
+        test_accuracy = accuracy_score(y_test, y_pred_test)
+        train_accuracy = accuracy_score(y_train, y_pred_train)
+        print(f'{model_name}:') 
+        print(f'\t Train accuracy: {train_accuracy:.4f}')
+        print(f'\t Test accuracy: {test_accuracy:.4f}')
+        acc_scores.append(test_accuracy)
+
+        # Regression task
+        regress_data_fn = args.regress_data_fn
+        x,y = get_embeddings(os.path.join(data_dir, regress_data_fn), 
+                             config, model, device, y_label='zspec', combine=combine)
+
+        # Splitting the dataset into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+        
+        # Creating and training a decision tree regressor
+        regressor = DecisionTreeRegressor(max_depth=None, random_state=42) # 6
+        regressor.fit(X_train, y_train)
+        
+        # Predicting the continuous values 
+        y_pred_test = regressor.predict(X_test)
+        y_pred_train = regressor.predict(X_train)
+        
+        # Evaluating the regressor
+        mse_test = mean_squared_error(y_test, y_pred_test)
+        mse_train = mean_squared_error(y_train, y_pred_train)
+        r2_test = r2_score(y_test, y_pred_test)
+        r2_train = r2_score(y_train, y_pred_train)
+        print(f'\t Train MSE: {mse_train:.4f}')
+        print(f'\t Test MSE: {mse_test:.4f}')
+        print(f'\t Train R2: {r2_train:.4f}')
+        print(f'\t Train R2: {r2_test:.4f}')
+        regress_scores.append(mse_test)
+    print(f'Best classifier model: {model_names[np.argmax(acc_scores)]}')
+    print(f'Best regression model: {model_names[np.argmin(regress_scores)]}')
