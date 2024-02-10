2d4870899a8c6937b3e28632a6f455e4f3a4908d
Teaghan O'Briain <obriaintb@gmail.com>
2024-02-10 14:21:08 +0000

    decision tree on embeddings

--- decision_tree.py
+++ decision_tree.py
@@ -111,7 +111,7 @@ for model_name in model_names:
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
     
     # Creating and training a decision tree classifier
-    clf = DecisionTreeClassifier(max_depth=None, random_state=42)
+    clf = DecisionTreeClassifier(max_depth=3, random_state=42)
     clf.fit(X_train, y_train)
     
     # Predicting the class labels of the test set
