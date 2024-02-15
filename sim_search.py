f992ef97ac56db70c78a6c87d229b099067e88f4
Teaghan O'Briain <obriaintb@gmail.com>
2024-02-15 18:11:38 +0000

    similarity augmentations

--- sim_search.py
+++ sim_search.py
@@ -150,9 +150,8 @@ test_dataloader = build_dataloader(os.path.join(data_dir, test_fn),
 target_latent, target_images = mae_latent(model, target_dataloader, device, return_images=True, apply_augmentations=augment_targets)
 
 if max_pool:
-    print(target_latent.shape)
+    print('Pooling')
     target_latent, _ = torch.max(target_latent, dim=1, keepdim=True)
-    print(target_latent.shape)
 
 #if target_indices is not None:
     # Only select hand-chosen sub-sample
