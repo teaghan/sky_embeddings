6179b2711de01a0044a59ce52fdd4b849d02e12b
Teaghan O'Briain <obriaintb@gmail.com>
2024-02-18 01:18:26 +0000

    lp central patches

--- linear_probe.py
+++ linear_probe.py
@@ -73,6 +73,7 @@ def get_embeddings(data_path, config, model, device, y_label='class', combine='p
         x = latent_features.reshape(latent_features.shape[0], -1)
     if combine=='central':
         x = select_centre(latent_features, n_patches=4)
+        print(x.shape)
         x = x.reshape(x.shape[0], -1)
     elif combine=='pool':
         x = np.max(latent_features, axis=1)
