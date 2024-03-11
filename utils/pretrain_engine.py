3a7a5f30868838ef720f32f4cb59720035d9413c
Teaghan O'Briain <obriaintb@gmail.com>
2024-03-11 01:59:09 +0000

    linreg in lp

--- utils/pretrain_engine.py
+++ utils/pretrain_engine.py
@@ -121,8 +121,8 @@ def linear_probe(encoder, losses_cp, device, dataloader_template, class_data_pat
         X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
         
         # Creating and training a linear model for regression
-        #regressor = LinearRegression()
-        regressor = ElasticNet(alpha=0.001, l1_ratio=0.9, max_iter=10000, random_state=42)
+        regressor = LinearRegression()
+        #regressor = ElasticNet(alpha=0.0001, l1_ratio=0.9, max_iter=10000, random_state=42)
         regressor.fit(X_train, y_train)
         
         # Predicting the continuous values 
