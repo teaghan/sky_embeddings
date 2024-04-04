13d2823d4b21900adb5c7d56d4aa617bd1e94c9b
Ashley Ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2024-04-03 22:25:17 -0700

    dwarf classification dset

--- utils/generate_unions_eval_dwarfs.py
+++ utils/generate_unions_eval_dwarfs.py
@@ -1,3 +1,4 @@
+# pretty inefficient but only needs to run once
 import numpy as np
 import h5py
 
@@ -7,8 +8,8 @@ cc_dataloader_path = '/github/extra/TileSlicer/'
 sys.path.insert(0, src+cc_dataloader_path)
 from dataloader import dataset_wrapper
 
-eval_dataset_path = '/home/a4ferrei/scratch/data/dr5_eval_set_redshift_6k.h5'
-eval_tiles = set(range(500))  # specify tiles to evaluate, only taking 6k of each
+eval_dataset_path = '/home/a4ferrei/scratch/data/dr5_eval_set_dwarfs_1k.h5' # lets see if we can fill 1k
+eval_tiles = set(range(50000))  
 
 # Initialize dataset wrapper
 dataset = dataset_wrapper()
@@ -16,11 +17,11 @@ dataset = dataset_wrapper()
 # Open HDF5 file for writing
 with h5py.File(eval_dataset_path, 'w') as f:
     # Create datasets
-    max_length = 6000
+    max_length = 1000
     dset_cutouts = f.create_dataset("cutouts", (max_length, 5, 224, 224), dtype=np.float32)
     dset_ra = f.create_dataset("ra", (max_length,), dtype='f')
     dset_dec = f.create_dataset("dec", (max_length,), dtype='f')
-    dset_zspec = f.create_dataset("zspec", (max_length,), dtype='f')
+    dset_dwarf = f.create_dataset("dwarf", (max_length,), dtype='f')
 
     # Initialize counters
     index = 1
@@ -31,21 +32,41 @@ with h5py.File(eval_dataset_path, 'w') as f:
         cutouts, catalog, tile = dataset.__next__()
 
         # Check if tile is in eval_tiles
-        #if tile in eval_tiles:
-        # index 0 gave an error so skipping for now
-        #if index > 0: # since not specifying specific tiles, just doing a range
         if tile != (285, 281):
             print('######', tile, index)
             tiles_written.append(tile)
 
             # Process catalog data and store directly in datasets
-            for i in range(len(catalog)):
-                zspec = catalog['zspec'].iloc[i]
-                if np.isfinite(zspec) and zspec > 0.002:
+            entries = len(catalog)
+            for i in range(entries):
+                lsb = catalog['lsb'].iloc[i]
+                gal = catalog['class'].iloc[i]
+                if lsb==1 and gal==2:
                     dset_cutouts[index] = cutouts[i] 
                     dset_ra[index] = catalog['ra'].iloc[i]
                     dset_dec[index] = catalog['dec'].iloc[i]
-                    dset_zspec[index] = zspec
+                    dset_dwarf[index] = 1
+                    # Increment index
+                    index += 1
+
+                    lsb = catalog['lsb'].iloc[i+1]
+                    gal = catalog['class'].iloc[i+1]
+                    if i+1 < entries and (lsb!=1 or gal!=2):
+                        dset_cutouts[index] = cutouts[i+1] 
+                        dset_ra[index] = catalog['ra'].iloc[i+1]
+                        dset_dec[index] = catalog['dec'].iloc[i+1]
+                        dset_dwarf[index] = 0
+                        # Increment index
+                        index += 1
+                    else:
+                        print('dwarf without a partner')
+
+
+                    # ADD NON-LSB after each one of these thinking they are similar...? 
+                    # check next one is not one or it is not last one --> maybe set up 30ks
+                    dset_cutouts[index+1] = cutouts[i+1] 
+                    dset_ra[index+1] = catalog['ra'].iloc[i+1]
+                    dset_dec[index+1] = catalog['dec'].iloc[i+1]
 
                     # Increment index
                     index += 1
