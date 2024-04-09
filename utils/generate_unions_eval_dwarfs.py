307686d8b99a5a90d15ce0ccda35ec7c817000cd
Ashley Ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2024-04-09 08:53:30 -0400

    Update generate_unions_eval_dwarfs.py

--- utils/generate_unions_eval_dwarfs.py
+++ utils/generate_unions_eval_dwarfs.py
@@ -8,19 +8,25 @@ dwarf_class_file = root_dir + 'dr5_eval_set_dwarfs_class.h5' # to create
 
 # Open dwarf_file and validation_file
 with h5py.File(dwarf_file, 'r') as dwarf_f, h5py.File(validation_file, 'r') as validation_f:
+    print('dwarfs')
     # Read data from dwarf_file
     dwarf_data = {}
     for key in dwarf_f.keys():
+        print(key)
         dwarf_data[key] = dwarf_f[key][:]
 
+    print('validation')
     # Read data from validation_file
     validation_data = {}
     for key in validation_f.keys():
+        print(key)
         validation_data[key] = validation_f[key][:]
+
+    key_0 = validation_f.keys()[0]
 
     # Randomly sample the same amount of data from the validation set
     validation_data_sampled = {}
-    sample_indices = random.sample(range(len(validation_data['zspec'])), len(dwarf_data['zspec']))
+    sample_indices = random.sample(range(len(validation_data[key_0])), len(dwarf_data[key_0]))
     for key in validation_data.keys():
         validation_data_sampled[key] = [validation_data[key][i] for i in sample_indices]
 
@@ -30,7 +36,7 @@ with h5py.File(dwarf_file, 'r') as dwarf_f, h5py.File(validation_file, 'r') as v
         combined_data[key] = dwarf_data[key] + validation_data_sampled[key]
 
     # Add is_dwarf key to indicate the source of the data
-    combined_data['is_dwarf'] = [1] * len(dwarf_data['zspec']) + [0] * len(validation_data_sampled['zspec'])
+    combined_data['is_dwarf'] = [1] * len(dwarf_data[key_0]) + [0] * len(validation_data_sampled[key_0])
 
 # Save combined data to dwarf_class_file
 with h5py.File(dwarf_class_file, 'w') as dwarf_class_f:
