cd4f2374521e577ab8ba57738beb40cb56d78c7a
Ashley Ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2024-04-14 18:58:06 -0400

    Update test_predictor.py

--- test_predictor.py
+++ test_predictor.py
@@ -74,7 +74,7 @@ def main(args):
     num_workers = min([os.cpu_count(),12*n_gpu])
 
     dataloader = build_unions_dataloader(batch_size=int(config['TRAINING']['batch_size']), 
-                                                num_workers=num_workers,
+                                                #num_workers=num_workers,
                                                 patch_size=int(mae_config['ARCHITECTURE']['patch_size']), 
                                                 num_channels=int(mae_config['ARCHITECTURE']['num_channels']), 
                                                 max_mask_ratio=0.0, eval=True,
@@ -84,11 +84,12 @@ def main(args):
                                                 eval_data_file=(config['DATA']['lp_regress_data_file_train']),
                                                 augment=str2bool(config['TRAINING']['augment']))
     
+    print(len(dataloader.dataset))
     train_val_idx, test_idx = train_test_split(range(len(dataloader.dataset)), test_size=0.2, random_state=42)
     train_idx, val_idx = train_test_split(train_val_idx, test_size=0.2, random_state=42) 
 
     dataloader_val = build_unions_dataloader(batch_size=int(config['TRAINING']['batch_size']),
-                                                num_workers=num_workers,
+                                                #num_workers=num_workers,
                                                 patch_size=int(mae_config['ARCHITECTURE']['patch_size']), 
                                                 num_channels=int(mae_config['ARCHITECTURE']['num_channels']), 
                                                 max_mask_ratio=0.0, eval=True,
@@ -96,7 +97,7 @@ def main(args):
                                                 num_patches=model.module.patch_embed.num_patches,
                                                 label_keys=eval(mae_config['DATA']['label_keys']),
                                                 eval_data_file=(config['DATA']['lp_regress_data_file_train']),
-                                                augment=str2bool(config['TRAINING']['augment']), indices=val_idx)
+                                                augment=str2bool(config['TRAINING']['augment']), indices=test_idx)
     
     print('The validation set consists of %i cutouts.' % (len(dataloader_val.dataset)))
 
@@ -105,10 +106,12 @@ def main(args):
                                             use_label_errs=str2bool(config['TRAINING']['use_label_errs']))
 
     snr_vals = h5_snr(h5_path=os.path.join(data_dir, config['DATA']['lp_regress_data_file_train']), 
-                      n_central_pix=8, batch_size=5000, num_samples=None)[val_idx]
+                      n_central_pix=8, batch_size=5000, num_samples=None)[test_idx]
     
     # Calculate minimum snr of the 5 channels
+    print(len(test_idx))
     print(snr_vals.shape)
+    print(pred_labels.shape, tgt_labels.shape) # not 0.2 of 6k loaded?
     snr = np.nanmin(snr_vals[:,:5], axis=(1))
     
     # Only display objects that are not super noisy
@@ -117,13 +120,19 @@ def main(args):
 
     if 'mse' in loss_fn.lower():
         plot_resid_hexbin([r'$Z$'], tgt_labels[snr_indices], pred_labels[snr_indices], y_lims=[1], 
+        #plot_resid_hexbin([r'$Z$'], tgt_labels, pred_labels, y_lims=[1], 
                           gridsize=(80,40), max_counts=5, cmap='ocean_r', n_std=4,
                           savename=os.path.join(fig_dir, f'{model_name}_predictions.png'))
         
+        #evaluate_z(pred_labels[snr_indices], tgt_labels[snr_indices], n_bins=8, z_range=(0.2,1.6), threshold=0.1, 
+        #           #y_lims=[(-0.08,0.08),(-0.02,0.02),(0,0.03),(0,0.1)], 
+        #           snr=snr[snr_indices],
+        #           savename=os.path.join(fig_dir, f'{model_name}_redshift.png'))
         evaluate_z(pred_labels[snr_indices], tgt_labels[snr_indices], n_bins=8, z_range=(0.2,1.6), threshold=0.1, 
                    #y_lims=[(-0.08,0.08),(-0.02,0.02),(0,0.03),(0,0.1)], 
                    snr=snr[snr_indices],
                    savename=os.path.join(fig_dir, f'{model_name}_redshift.png'))
+        
     else:
         # Turn logit predictions into classes
         pred_class = np.argmax(pred_labels, 1)
