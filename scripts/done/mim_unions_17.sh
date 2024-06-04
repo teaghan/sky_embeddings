35ceda050673a48c630907ce84e259cbef32135f
Ashley Ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2024-06-04 00:13:34 -0400

    217

---
+++ scripts/done/mim_unions_17.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+module load StdEnv/2020
+module load gcc/9.3.0 arrow/13.0.0 python/3.10
+source /home/a4ferrei/mae_env/bin/activate
+module load hdf5/1.10.6
+
+# Run MAE training
+python /home/a4ferrei/scratch/github/sky_embeddings/pretrain_mim.py mim_213_unions -v 2 -ct 60.00
