7661787973856dfd0d88c3bc33b1c03074dedcce
heesters-nick <nick.heesters@epfl.ch>
2024-06-14 11:42:32 -0400

    update env activation script

---
+++ scripts/done/activate_with_modules.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+module load StdEnv/2023
+module load gcc/12.3 arrow/16.1.0 python/3.11.5 hdf5/1.14.2
+source /home/heesters/projects/def-sfabbro/heesters/envs/ssl_env/bin/activate
--- scripts/done/mim_unions.sh
+++ scripts/done/mim_unions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 module load StdEnv/2023
-module load gcc/9.3.0 arrow/13.0.0 python/3.11.5
-source /home/heesters/projects/def-sfabbro/heesters/envs/SSL/bin/activate
-module load hdf5/1.10.6
+module load gcc/12.3 arrow/16.1.0 python/3.11.5
+source /home/heesters/projects/def-sfabbro/heesters/envs/ssl_env/bin/activate
+module load hdf5/1.14.2
 
 # Run MAE training
 python /home/heesters/projects/def-sfabbro/heesters/github/sky_embeddings/pretrain_mim.py mim_218_unions -v 1 -ct 1.00
