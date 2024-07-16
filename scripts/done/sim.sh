42326cad02922f1adc02c070e7c878057ac479ad
Ashley Ferreira <a4ferrei@narval2.narval.calcul.quebec>
2024-07-16 12:59:52 -0400

    I think all general paths but code execution points to a4ferrei home

--- scripts/done/sim.sh
+++ scripts/done/sim.sh
@@ -2,14 +2,14 @@
 
 # Module loads
 module load python/3.11.5
-source /home/a4ferrei/projects/def-sfabbro/a4ferrei/envs
+source /projects/def-sfabbro/a4ferrei/envs
 /mae_env/bin/activate
 module load hdf5/1.10.6
 
 # Copy files to slurm directory
-cp /lustre07/scratch/obriaint/sky_embeddings/cc/../data/HSC_dwarf_galaxies_GRIZY_64_new.h5 $SLURM_TMPDIR
-cp /lustre07/scratch/obriaint/sky_embeddings/cc/../data/HSC_unkown_GRIZY_64_new.h5 $SLURM_TMPDIR
-cp /lustre07/scratch/obriaint/sky_embeddings/cc/../data/HSC_strong_lens_candidates_GRIZY_64.h5 $SLURM_TMPDIR
+cp /project/rrg-kyi/astro/hsc/HSC_dwarf_galaxies_GRIZY_64_new.h5 $SLURM_TMPDIR
+cp /project/rrg-kyi/astro/hsc/HSC_unkown_GRIZY_64_new.h5 $SLURM_TMPDIR
+cp /project/rrg-kyi/astro/hsc/HSC_strong_lens_candidates_GRIZY_64.h5 $SLURM_TMPDIR
 
 python /lustre07/scratch/obriaint/sky_embeddings/cc/../similarity_search.py sim_22 -dd $SLURM_TMPDIR/ -bs 256
 
--- scripts/todo/cls_train_samples.sh
+++ scripts/todo/cls_train_samples.sh
@@ -2,13 +2,13 @@
 
 # Module loads
 module load python/3.11.5
-source /home/a4ferrei/projects/def-sfabbro/a4ferrei/envs
+source /projects/def-sfabbro/a4ferrei/envs
 /mae_env/bin/activate
 module load hdf5/1.10.6
 
 # Copy files to slurm directory
-cp /home/a4ferrei/projects/rrg-kyi/a4ferrei/july_2024/sky_embeddingscc/../data/HSC_dud_classes_calexp_GIRYZ7610_64_train.h5 $SLURM_TMPDIR
-cp /home/a4ferrei/projects/rrg-kyi/a4ferrei/july_2024/sky_embeddingscc/../data/HSC_dud_classes_calexp_GIRYZ7610_64_val.h5 $SLURM_TMPDIR
+cp /project/rrg-kyi/astro/hsc/HSC_dud_classes_calexp_GIRYZ7610_64_train.h5 $SLURM_TMPDIR
+cp /project/rrg-kyi/astro/hsc/HSC_dud_classes_calexp_GIRYZ7610_64_val.h5 $SLURM_TMPDIR
 
 # Run predictor training
 python /home/a4ferrei/projects/rrg-kyi/a4ferrei/july_2024/sky_embeddingscc/../train_predictor.py cls_ap_025k -v 1 -ct 10.00 -dd $SLURM_TMPDIR/
