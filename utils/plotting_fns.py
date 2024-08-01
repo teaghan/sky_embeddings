e7419e7eac8ba6334aab9a7c0c7d35e66867aadd
Teaghan O'Briain <obriaintb@gmail.com>
2024-08-01 02:53:58 +0000

    new plot

--- utils/plotting_fns.py
+++ utils/plotting_fns.py
@@ -168,6 +168,41 @@ def plot_batch(orig_imgs, mask_imgs, pred_imgs,
 
     plt.close()
 
+def plot_batch_orig_pred(orig_imgs, pred_imgs, 
+                         n_samples=5, savename=None):
+    
+    # Normalize the batch between 0 and 1
+    orig_imgs = normalize_images(np.concatenate((orig_imgs, pred_imgs)))
+    b = pred_imgs.shape[0]
+    pred_imgs = orig_imgs[b:b*2]
+    orig_imgs = orig_imgs[:b]
+
+    # Create a figure with subplots
+    fig, axes = plt.subplots(n_samples, 2, figsize=(5, n_samples*5/3))
+    
+    # Loop through the images and display each one
+    for i in range(n_samples):
+        if i==0:
+            axes[i,0].set_title('Target', fontsize=12)
+            axes[i,2].set_title('Generated', fontsize=12)
+        vmin = np.min(orig_imgs[i])
+        vmax = np.max(orig_imgs[i])
+        axes[i,0].imshow(orig_imgs[i,0], vmin=vmin, vmax=vmax)
+        axes[i,1].imshow(pred_imgs[i,0], vmin=vmin, vmax=vmax)
+        for j in range(3):
+            axes[i,j].axis('off')  # Hide the axes
+
+    plt.tight_layout()
+    
+    if savename is not None:
+        plt.savefig(savename, facecolor='white', transparent=False, dpi=100,
+                    bbox_inches='tight', pad_inches=0.05)
+    
+    else:
+        plt.show()
+
+    plt.close()
+
 def tile_channels(image, grid_size=None):
     """
     Rearranges the channels of an image into a tiled grid on a single axis.
